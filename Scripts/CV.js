1f73c23fb0b40e0e3e5165920d11e835862df776
Terry Kheswa <romeokheswa@gmail.com>
2025-06-06 16:04:28 +0200

    Update CV.js
    added CV images to the repository and renamed their file paths

--- Scripts/CV.js
+++ Scripts/CV.js
@@ -8,12 +8,12 @@ cvButton.addEventListener('click', () => {
         <div class="CV-display-block">
           <h2 class="CV-title-1">MY RESUME</h2>
           <h2 class="CV-title">CURRICULUM VITAE</h2>
-          <img src="Images/CV-images/CV-01.jpg" alt="" class="cv-image">
+          <img src="CV-images/CV-01.jpg" alt="" class="cv-image">
           <h2 class="CV-title">NATIONAL SENIOR CERTIFICATE</h2>
-          <img src="Images/CV-images/CV-02.jpg" alt="" class="cv-image">
+          <img src="CV-images/CV-02.jpg" alt="" class="cv-image">
           <h2 class="CV-title">NQF QUALIFICATIONS</h2>
-          <img src="Images/CV-images/CV-03.jpg" alt="" class="cv-image">
-          <img src="Images/CV-images/CV-04.jpg" alt="" class="cv-image">
+          <img src="CV-images/CV-03.jpg" alt="" class="cv-image">
+          <img src="CV-images/CV-04.jpg" alt="" class="cv-image">
         </div>
   `
 });
